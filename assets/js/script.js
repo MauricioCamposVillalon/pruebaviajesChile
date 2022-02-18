ca38dd400531eaee85fbc3549a67dbde0b9dcb69
Mauricio Campos <thoyferr@gmail.com>
2022-02-18 18:18:02 -0300

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 
-
+/* Java de conexion con Typeform */
 $(document).ready(function () {
     var qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement,
-    gt = d.getElementsByTagName, id = "typef_orm",
-    b = "https://embed.typeform.com/"; if (!gi.call(d, id)) {
-        js = ce.call(d, "script"); js.id = id; js.src = b + "embed.js";
-        q = gt.call(d, "script")[0]; q.parentNode.insertBefore(js, q)
-    }
-   
-    
+        gt = d.getElementsByTagName, id = "typef_orm",
+        b = "https://embed.typeform.com/"; if (!gi.call(d, id)) {
+            js = ce.call(d, "script"); js.id = id; js.src = b + "embed.js";
+            q = gt.call(d, "script")[0]; q.parentNode.insertBefore(js, q)
+        }
+
+
 
 });
 
+/* inicio de tooltip de inicio de pagina */
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-  })
+})
